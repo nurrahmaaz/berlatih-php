58fc8b8810f7e71c0aa1403930fcbc9a63c06ad9
nurrahmaaz <ammaazharii@gmail.com>
2020-07-30 08:41:46 +0800

    berubah

--- papan-catur.php
+++ papan-catur.php
@@ -2,18 +2,30 @@
 
 function papan_catur($angka) {
 // tulis kode di sini
+	for ($i=0; $i < $angka; $i++) { 
+		for ($j=0; $j < ($angka*2); $j++) { 
+			if ($j%2==1 && $i%2==1) {
+				echo "#&nbsp";
+			}
+			elseif ($j%2==0 && $i%2 == 0) {
+				echo "#&nbsp";
+			} else {
+				echo "&nbsp";
+			}
+		}
+		echo "<br>";
+	}
 }
 
 // TEST CASES
-echo papan_catur(4) 
-/*
+echo papan_catur(4). "<br><br>" ;
 # # # #
  # # #
 # # # #
  # # #
- */
+ 
 
-echo papan_catur(8)
+echo papan_catur(8) . "<br><br>";
 /* 
 # # # # # # # #
  # # # # # # # 
@@ -23,8 +35,8 @@ echo papan_catur(8)
  # # # # # # #
 # # # # # # # #
  # # # # # # #
-
-echo papan_catur(5) 
+*/
+echo papan_catur(5) . "<br><br>";
 /*
 # # # # #
  # # # #
